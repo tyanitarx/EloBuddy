89bed06080c21f3626f2ca4c82e2caf584b45012
tyanitarx <demonlit1@hotmail.com>
2016-01-09 18:53:51 -0500

    Create Program.cs

---
+++ EloBuddy-Test/EloBuddy-Test/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX;
+using EloBuddy;
+using EloBuddy.SDK;
+
+namespace EloBuddy_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Clone_Movement();
+        }
+    }
+}
